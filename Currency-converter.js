18d8e0e858f1d89669ad8956d4d64d05a3213471
gradhav001 <jesse.richard@strathmore.edu>
2023-09-02 18:19:23 +0300

    improved the usability of the application by allowing users to get the exchange rate for certain amount that they enter

--- Currency-converter.js
+++ Currency-converter.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     let convertBtn = document.querySelector('.btn');
+    let amountToConvert = document.querySelector('#amount');
 
     convertBtn.addEventListener('click', function () {
         fetch('https://api.exchangerate-api.com/v4/latest/USD')
@@ -8,11 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(data => {
                 let currency = document.querySelector('#currency').value.toUpperCase();
                 console.log(currency);
-                 
                 let rate = data.rates[currency];
+                let amount =amountToConvert.value;
+                let exchange = rate*amount;
+
+                 
                 console.log(rate);
                 if (rate !== undefined) {
-                    document.querySelector('#result').innerHTML = '1 USD is equal to' + rate + currency;
+                    document.querySelector('#result').innerHTML = `${amount} dollars is equal to ` + exchange + currency;
                 } else {
                     document.querySelector('#result').innerHTML = "invalid currency";
                 }
