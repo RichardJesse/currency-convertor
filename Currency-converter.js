bdff2573222a21497b1660ff5e91d7a8dfcd5699
gradhav001 <jesse.richard@strathmore.edu>
2023-09-09 18:52:02 +0300

    found a way to get only the value attributes of the option the user selects using the getAttibute() method

--- Currency-converter.js
+++ Currency-converter.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // let convertBtn = document.querySelector('.btn');
     let amountToConvert = document.querySelector('#amount');
     
-
+ 
     amountToConvert.addEventListener('input', function (event) {
         fetch('https://api.exchangerate-api.com/v4/latest/USD')
 
@@ -11,13 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(data => {
                 var selectElement = document.getElementById('inputGroupSelect01');
                 var selectedOption = selectElement.options[selectElement.selectedIndex];
-                var currency = selectedOption.text.toUpperCase();
+                var currency = selectedOption.getAttribute("value");
                 
                 
                 let rate = data.rates[currency];
                 let amount =amountToConvert.value;
                 let exchange = rate*amount;
-
+               console.log(selectElement);
+               console.log(selectedOption);
+               console.log(currency);
 
                  
                 console.log(rate);
