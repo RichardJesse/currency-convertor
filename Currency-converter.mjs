a271651eb9d5c410cee8e15b975f3fa0d02e1925
gradhav001 <jesse.richard@strathmore.edu>
2023-09-22 17:45:44 +0300

    reverted to the previous API since there was no API that offered the use of their api to convert frrely without a particular base currency

--- Currency-converter.mjs
+++ Currency-converter.mjs
@@ -1,56 +1,44 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     // let convertBtn = document.querySelector('.btn');
-//     let amountToConvert = document.querySelector('#amount');
+document.addEventListener('DOMContentLoaded', function () {
+    // let convertBtn = document.querySelector('.btn');
+    let amountToConvert = document.querySelector('#amount');
     
  
-//     amountToConvert.addEventListener('input', function (event) {
-//         fetch('https://api.exchangerate-api.com/v4/latest/USD')
+    amountToConvert.addEventListener('input', function (event) {
+        fetch('https://api.exchangerate-api.com/v4/latest/USD')
 
-//             .then(response => { return response.json()})
-//             .then(data => {
-//                 var selectElement = document.getElementById('inputGroupSelect01');
-//                 var selectedOption = selectElement.options[selectElement.selectedIndex];
-//                 var currency = selectedOption.getAttribute("value");
+            .then(response => { return response.json()})
+            .then(data => {
+                var selectElement = document.getElementById('inputGroupSelect01');
+                var selectedOption = selectElement.options[selectElement.selectedIndex];
+                var currency = selectedOption.getAttribute("value");
                 
                 
-//                 let rate = data.rates[currency];
-//                 let amount =amountToConvert.value;
-//                 let exchange = rate*amount;
-//                console.log(selectElement);
-//                console.log(selectedOption);
-//                console.log(currency);
+                let rate = data.rates[currency];
+                let amount =amountToConvert.value;
+                let exchange = rate*amount;
+               
 
                  
-//                 console.log(rate);
-//                 if (rate !== undefined) {
-//                     document.querySelector('#convertedRate').value = exchange;
+                console.log(rate);
+                if (rate !== undefined) {
+                    document.querySelector('#convertedRate').value = exchange;
                     
-//                 } else {
-//                     document.querySelector('#convertedRate').value = "invalid currency";
-//                 }
+                } else {
+                    document.querySelector('#convertedRate').value = "invalid currency";
+                }
 
 
-//                 return false;
-
-
-
-
-//             });
-//     })
-// })
-// allow for realtime conversion of the currencies as in the time a user inputs a value it is converted
+                return false;
 
 
 
-// input.setCustomValidity("Select dfhbfhdfbhjn")
-import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
-const freecurrencyapi = new Freecurrencyapi('fca_live_feWykViIGhAvzc5Sg6E9UFIdV5pdnLtVsj1QKjgE');
+            });
+    })
+})
 
-freecurrencyapi.latest({
-    base_currency: 'USD',
-    currencies: 'EUR'
-}).then(response => {
-    console.log(response);
-});
+
+
+
+
